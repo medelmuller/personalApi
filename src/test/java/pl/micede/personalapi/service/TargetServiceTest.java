9eec5505b7ebe10a99de4f2d502bddcc105efc40
medelmuller <m.edelmuller@gmail.com>
2024-03-26 20:12:15 +0100

    6. Target Service tests

--- src/test/java/pl/micede/personalapi/service/TargetServiceTest.java
+++ src/test/java/pl/micede/personalapi/service/TargetServiceTest.java
@@ -1,23 +1,38 @@
 package pl.micede.personalapi.service;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import pl.micede.personalapi.dto.TargetReadDto;
 import pl.micede.personalapi.dto.TargetReqDto;
+import pl.micede.personalapi.model.TargetCategory;
 import pl.micede.personalapi.model.TargetModel;
 import pl.micede.personalapi.repository.TargetRepository;
 import pl.micede.personalapi.utils.mapper.TargetMapper;
 
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
 class TargetServiceTest {
 
     @Mock
     private TargetRepository targetRepository;
 
-    @Mock
+    @InjectMocks
     private TargetMapper targetMapper;
 
     @Mock
@@ -26,20 +41,81 @@ class TargetServiceTest {
     @InjectMocks
     private TargetService targetService;
 
-    @Test
-    void addNewTarget_ShouldSaveNewTarget() {
-        TargetModel targetModel = new TargetModel();
-        targetModel.setTargetName("targetName");
-        targetModel.setDescription("description");
-//        targetModel.setTargetBegins(2024-04-20T22:22:22);
+    @BeforeEach
+    void setUp() {
+        reqDto = TargetReqDto.builder()
+                .targetName("Study")
+                .description("Study harder for final project")
+                .targetCategory(TargetCategory.KNOWLEDGE)
+                .targetBegins(LocalDateTime.of(2024, Month.JUNE, 1, 12, 0))
+                .targetEnds(LocalDateTime.of(2024, Month.JULY, 3, 12, 0))
+                .habits(new ArrayList<>())
+                .build();
     }
 
     @Test
-    void getTargetById() {
+    void addNewTarget_ShouldSaveNewTarget() {
+        TargetModel targetModel = new TargetModel();
+        targetModel.setTargetName(reqDto.getTargetName());
+        targetModel.setDescription(reqDto.getDescription());
+        targetModel.setTargetBegins(reqDto.getTargetBegins());
+        targetModel.setTargetEnds(reqDto.getTargetEnds());
+        targetModel.setTargetCategory(reqDto.getTargetCategory());
+        targetModel.setHabits(reqDto.getHabits());
+
+
+//        BDDMockito.when(targetRepository.save(targetModel)).thenReturn(new TargetModel());
+
+        targetService.addNewTarget(reqDto);
+        verify(targetRepository).save(any(TargetModel.class));
+    }
+
+    @Test
+    void getTargetById_ShouldFindTargetById() {
+        Long id = 1L;
+        TargetModel targetModel = new TargetModel();
+        targetModel.setTargetName(reqDto.getTargetName());
+        targetModel.setDescription(reqDto.getDescription());
+        targetModel.setTargetBegins(reqDto.getTargetBegins());
+        targetModel.setTargetEnds(reqDto.getTargetEnds());
+        targetModel.setTargetCategory(reqDto.getTargetCategory());
+        targetModel.setHabits(reqDto.getHabits());
+
+        when(targetRepository.findById(id)).thenReturn(Optional.of(targetModel));
+        when(targetRepository.save(any(TargetModel.class))).thenReturn(targetModel);
+
+
+        TargetReadDto result = targetService.getTargetById(id);
+        assertEquals(targetModel.getId(), result.getId());
+    }
+
+    @Test
+    void getTargetById_ShouldThrowAnException() {
+
     }
 
     @Test
     void getTargetsByCategory() {
+        TargetCategory category = TargetCategory.KNOWLEDGE;
+        TargetModel targetModel = new TargetModel();
+        targetModel.setTargetName(reqDto.getTargetName());
+        targetModel.setDescription(reqDto.getDescription());
+        targetModel.setTargetBegins(reqDto.getTargetBegins());
+        targetModel.setTargetEnds(reqDto.getTargetEnds());
+        targetModel.setTargetCategory(reqDto.getTargetCategory());
+        targetModel.setHabits(reqDto.getHabits());
+
+
+        List<TargetReadDto> collect = List.of(targetModel).stream().map(targetMapper::toDto).collect(Collectors.toList());
+
+        when(targetRepository.findAllByTargetCategory(category)).thenReturn(List.of(targetModel));
+        when(targetRepository.save(any(TargetModel.class))).thenReturn(targetModel);
+
+        List<TargetReadDto> result = targetService.getTargetsByCategory(String.valueOf(category));
+
+
+
+        assertEquals(collect , result);
     }
 
     @Test
