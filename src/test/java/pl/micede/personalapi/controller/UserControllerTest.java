ba0403d0257f7fc7ea131bd899dbb304a595491c
medelmuller <m.edelmuller@gmail.com>
2024-04-18 16:14:27 +0200

    18. Minor changes

---
+++ src/test/java/pl/micede/personalapi/controller/UserControllerTest.java
@@ -0,0 +1,24 @@
+package pl.micede.personalapi.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+
+    @Test
+    void addNewUser() {
+    }
+
+    @Test
+    void getUserById() {
+    }
+
+    @Test
+    void updateUserPasswordByLogin() {
+    }
+
+    @Test
+    void deleteUserById() {
+    }
+}
